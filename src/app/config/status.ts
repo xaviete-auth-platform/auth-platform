b9401a1b983e65f99fb46e369afac243b30954e8
xavigrausebastian <xavier.grau@kreatings.com>
2022-08-19 20:02:35 +0200

    ADD status file

---
+++ src/app/config/status.ts
@@ -0,0 +1,18 @@
+// Http Status Codes
+
+export const HttpStatusCode = {
+  OK                    : 200,
+  CREATED               : 201,
+  ACCEPTED              : 202,
+  NOT_MODIFIED          : 304,
+  BAD_REQUEST           : 400,
+  UNAUTHORIZED          : 401,
+  FORBIDDEN             : 403,
+  NOT_FOUND             : 404,
+  METHOD_NOT_ALLOWED    : 405,
+  NOT_ACCEPTABLE        : 406,
+  REQUEST_TIMEOUT       : 408,
+  CONFLICT              : 409,
+  TOO_MANY_REQUESTS     : 429,
+  INTERNAL_SERVER_ERROR : 500
+};
